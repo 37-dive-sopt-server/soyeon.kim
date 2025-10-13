3240b701319d623d34ea43910c156c19d151b54a
kaswhy <sxshx818@naver.com>
2025-10-13 21:39:58 +0900

    feat(member): 회원 도메인에 생년월일, 이메일, 성별 추가

--- src/main/java/org/sopt/member/domain/Member.java
+++ src/main/java/org/sopt/member/domain/Member.java
@@ -1,13 +1,55 @@
 package org.sopt.member.domain;
 
+import java.time.Instant;
+import java.time.LocalDate;
+
 public class Member {
 
     private Long id;
     private String name;
+    private LocalDate birthday;
+    private String email;
+    private Gender gender;
+    private Instant createdAt;
+    private Instant updatedAt;
 
-    public Member(Long id, String name) {
+    private Member(
+        Long id,
+        String name,
+        LocalDate birthday,
+        String email,
+        Gender gender,
+        Instant createdAt,
+        Instant updatedAt
+    ) {
         this.id = id;
         this.name = name;
+        this.birthday = birthday;
+        this.email = email;
+        this.gender = gender;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static Member of(
+        String name,
+        LocalDate birthday,
+        String email,
+        Gender gender
+    ) {
+       return new Member(null, name, birthday, email, gender, null, null);
+    }
+
+    public void updateId(Long id) {
+        this.id = id;
+    }
+
+    public void updateCreatedAt(Instant now) {
+        this.createdAt = now;
+    }
+
+    public void updateUpdatedAt(Instant now) {
+        this.updatedAt = now;
     }
 
     public Long getId() {
