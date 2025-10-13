272106aae7711be534caec03647b7907f3c2a171
kaswhy <sxshx818@naver.com>
2025-10-13 21:40:27 +0900

    refactor(member): 리포지토리 구현체, 인터페이스 분리

---
+++ src/main/java/org/sopt/member/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package org.sopt.member.repository;
+
+import org.sopt.member.domain.Member;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member entity);
+
+    Optional<Member> findById(Long id);
+
+    List<Member> findAll();
+}
--- src/main/java/org/sopt/member/repository/MemoryMemberRepository.java
+++ src/main/java/org/sopt/member/repository/MemoryMemberRepository.java
@@ -1,25 +1,34 @@
 package org.sopt.member.repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.time.Instant;
 import org.sopt.member.domain.Member;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
-public class MemoryMemberRepository {
+public class MemoryMemberRepository implements MemberRepository {
 
-    private static final Map<Long, Member> store = new HashMap<>();
+    private final Map<Long, Member> store = new ConcurrentHashMap<>();
+    private final AtomicLong sequence = new AtomicLong(1);
 
+    @Override
     public Member save(Member member) {
+        Instant now = Instant.now();
+        if (member.getId() == null) {
+            member.updateId(sequence.getAndIncrement());
+            member.updateCreatedAt(now);
+        }
+        member.updateUpdatedAt(now);
         store.put(member.getId(), member);
         return member;
     }
 
+    @Override
     public Optional<Member> findById(Long id) {
         return Optional.ofNullable(store.get(id));
     }
 
+    @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
