1bae92d817d6825355d494fa8a98507a4061854c
kaswhy <sxshx818@naver.com>
2025-10-13 18:42:05 +0900

    refactor(member): 컨트롤러에서 서비스 인터페이스를 의존하도록 변경

--- src/main/java/org/sopt/member/controller/MemberController.java
+++ src/main/java/org/sopt/member/controller/MemberController.java
@@ -3,22 +3,22 @@ package org.sopt.member.controller;
 import java.util.List;
 import java.util.Optional;
 import org.sopt.member.domain.Member;
+import org.sopt.member.service.MemberService;
 import org.sopt.member.service.MemberServiceImpl;
 
 public class MemberController {
 
-    private MemberServiceImpl memberServiceImpl = new MemberServiceImpl();
+    private MemberService memberService = new MemberServiceImpl();
 
     public Long createMember(String name) {
-
-        return memberServiceImpl.join(name);
+        return memberService.join(name);
     }
 
     public Optional<Member> findMemberById(Long id) {
-        return memberServiceImpl.findOne(id);
+        return memberService.findOne(id);
     }
 
     public List<Member> getAllMembers() {
-        return memberServiceImpl.findAllMembers();
+        return memberService.findAllMembers();
     }
 }
