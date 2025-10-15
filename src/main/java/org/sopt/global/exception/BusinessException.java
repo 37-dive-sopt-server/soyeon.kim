2f5c03927c279121121a77a3f51683cbae5ae04d
kaswhy <sxshx818@naver.com>
2025-10-15 17:49:52 +0900

    chore(glboal): BusinessException 인스턴스 변수명 오타 수정

--- src/main/java/org/sopt/global/exception/BusinessException.java
+++ src/main/java/org/sopt/global/exception/BusinessException.java
@@ -4,9 +4,9 @@ public class BusinessException extends RuntimeException {
 
     private final ErrorCode errorCode;
 
-    public BusinessException(ErrorCode erorCode) {
-        super(erorCode.getMessage());
-        this.errorCode = erorCode;
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
     }
 
     public ErrorCode getErrorCode() {
