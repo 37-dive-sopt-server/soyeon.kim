7eceb8d4069749f689a6ad7c541f7c860484a8ff
kaswhy <sxshx818@naver.com>
2025-10-15 15:42:33 +0900

    refactor(member): 멤버 생성 요청 dto 추가

--- src/main/java/org/sopt/member/api/dto/request/MemberCreateRequest.java
+++ src/main/java/org/sopt/member/api/dto/request/MemberCreateRequest.java
@@ -7,12 +7,4 @@ public record MemberCreateRequest(
     String gender
 ) {
 
-    public static MemberCreateRequest of(
-        String name,
-        String birthday,
-        String email,
-        String gender
-    ) {
-        return new MemberCreateRequest(name, birthday, email, gender);
-    }
 }
